84d26b70d3da1bf5dbe956365244d79d40d8f09d
VASANTH_3105 <vasanth.it2125@gmail.com>
2024-06-06 22:35:32 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import Stack from "@mui/material/Stack";
+
+export default function Hero() {
+  return (
+    <>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+
+    <Card sx={{ maxWidth: "800px" }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image=""
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+   
+
+      </Stack>
+    </>
+  );
+}
